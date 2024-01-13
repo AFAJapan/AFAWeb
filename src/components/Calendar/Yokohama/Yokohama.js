7c59e174646d8d5f18545548aa91cfd8aae8f7a9
AFAJapan <154581944+AFAJapan@users.noreply.github.com>
2024-01-14 00:52:42 +0900

    Update Yokohama.js Calendar

--- src/components/Calendar/Yokohama/Yokohama.js
+++ src/components/Calendar/Yokohama/Yokohama.js
@@ -2,182 +2,59 @@ import React from 'react';
 import '../../../styles/TokyoAcademy.css';
 import { Grid } from '@mui/material';
 
-export default function Yokohama() {
+export default function TokyoAcademy() {
+  // Schedules based on the provided table
+  const schedules = {
+    'January': {
+      'Academy': [
+        { date: 'Date  ', time: 'Time  ', location: 'Location  ' },
+        { date: '6-Jan-2024  ', time: '8:00-11:00 ', location: 'Shin-Yokohama park, Touteki-jyou' },
+        { date: '13-Jan-2024  ', time: '9:00-11:00', location: 'Grandberry' },
+        { date: '20-Jan-2024  ', time: '9:00-11:00', location: 'Grandberry' },
+        { date: '27-Jan-2024  ', time: '9:00-11:00', location: 'Grandberry' },
+      ],
+    },
+    'February': {
+      'Academy': [
+        { date: 'Date  ', time: 'Time  ', location: 'Location' },
+       
+      ],
+    },
+    'March': {
+      'Academy': [
+        { date: 'Date  ', time: 'Time  ', location: 'Location' },
+       
+      ],
+    },
+  };
+
+  const renderSchedule = (teamName, teamSchedule) => (
+    <>
+      <div className='team_name' style={{ fontSize: '20px' }}><strong>{`AFA Yokohama ${teamName}`}</strong></div>
+      {teamSchedule.map((session, index) => (
+        <div className='table_row' key={`${teamName}-${index}`} style={{ display: 'flex' }}>
+          <div className='table_cell' style={{ flex: '1' }}>{session.date}</div>
+          <div className='table_cell' style={{ flex: '1' }}>{session.time}</div>
+          <div className='table_cell' style={{ flex: '1' }}>{session.location}</div>
+        </div>
+      ))}
+    </>
+  );
+
+  const renderMonthSection = (month) => (
+    <div className='month_section'>
+      <div className='table_header'>{month} 2024</div>
+      {Object.entries(schedules[month]).map(([teamName, teamSchedule]) => (
+        <React.Fragment key={teamName}>
+          {renderSchedule(teamName, teamSchedule)}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+
   return (
     <div className='table_container'>
-        <div>
-            <div className='table_header'>OCTOBER 2023</div>
-            <Grid container className='table_row'>
-              <Grid item lg={3}>
-              AFA Yokohama Senior Kids (12 to 16 Years)
-              </Grid>
-              <Grid item lg={3}>
-              Date
-              </Grid>
-              <Grid item lg={3}>
-              Time
-              </Grid>
-              <Grid item lg={3}>
-              Locations
-              </Grid>
-            </Grid>
-            <Grid container className='table_row'>
-              <Grid item lg={3}>
-              AFA Yokohama Junior Kids J1 (8 to 12 Years)
-              </Grid>
-              <Grid item lg={3}>
-              Date
-              </Grid>
-              <Grid item lg={3}>
-              Time
-              </Grid>
-              <Grid item lg={3}>
-              Locations
-              </Grid>
-            </Grid>
-            <Grid container className='table_row'>
-              <Grid item lg={3}>
-              AFA Yokohama Junior Kids J2 (8 to 12 Years)
-              </Grid>
-              <Grid item lg={3}>
-              Date
-              </Grid>
-              <Grid item lg={3}>
-              Time
-              </Grid>
-              <Grid item lg={3}>
-              Locations
-              </Grid>
-            </Grid>
-            <Grid container className='table_row'>
-              <Grid item lg={3}>
-              AFA Yokohama Sub Junior Kids (4 to 8 Years)
-              </Grid>
-              <Grid item lg={3}>
-              Date
-              </Grid>
-              <Grid item lg={3}>
-              Time
-              </Grid>
-              <Grid item lg={3}>
-              Locations
-              </Grid>
-            </Grid>
-            <div className='table_header'>NOVEMBER 2023</div>
-            <Grid container className='table_row'>
-              <Grid item lg={3}>
-              AFA Yokohama Senior Kids (12 to 16 Years)
-              </Grid>
-              <Grid item lg={3}>
-              Date
-              </Grid>
-              <Grid item lg={3}>
-              Time
-              </Grid>
-              <Grid item lg={3}>
-              Locations
-              </Grid>
-            </Grid>
-            <Grid container className='table_row'>
-              <Grid item lg={3}>
-              AFA Yokohama Junior Kids J1 (8 to 12 Years)
-              </Grid>
-              <Grid item lg={3}>
-              Date
-              </Grid>
-              <Grid item lg={3}>
-              Time
-              </Grid>
-              <Grid item lg={3}>
-              Locations
-              </Grid>
-            </Grid>
-            <Grid container className='table_row'>
-              <Grid item lg={3}>
-              AFA Yokohama Junior Kids J2 (8 to 12 Years)
-              </Grid>
-              <Grid item lg={3}>
-              Date
-              </Grid>
-              <Grid item lg={3}>
-              Time
-              </Grid>
-              <Grid item lg={3}>
-              Locations
-              </Grid>
-            </Grid>
-            <Grid container className='table_row'>
-              <Grid item lg={3}>
-              AFA Yokohama Sub Junior Kids (4 to 8 Years)
-              </Grid>
-              <Grid item lg={3}>
-              Date
-              </Grid>
-              <Grid item lg={3}>
-              Time
-              </Grid>
-              <Grid item lg={3}>
-              Locations
-              </Grid>
-            </Grid>
-            <div className='table_header'>DECEMBER 2023</div>
-            <Grid container className='table_row'>
-              <Grid item lg={3}>
-              AFA Yokohama Senior Kids (12 to 16 Years)
-              </Grid>
-              <Grid item lg={3}>
-              Date
-              </Grid>
-              <Grid item lg={3}>
-              Time
-              </Grid>
-              <Grid item lg={3}>
-              Locations
-              </Grid>
-            </Grid>
-            <Grid container className='table_row'>
-              <Grid item lg={3}>
-              AFA Yokohama Junior Kids J1 (8 to 12 Years)
-              </Grid>
-              <Grid item lg={3}>
-              Date
-              </Grid>
-              <Grid item lg={3}>
-              Time
-              </Grid>
-              <Grid item lg={3}>
-              Locations
-              </Grid>
-            </Grid>
-            <Grid container className='table_row'>
-              <Grid item lg={3}>
-              AFA Yokohama Junior Kids J2 (8 to 12 Years)
-              </Grid>
-              <Grid item lg={3}>
-              Date
-              </Grid>
-              <Grid item lg={3}>
-              Time
-              </Grid>
-              <Grid item lg={3}>
-              Locations
-              </Grid>
-            </Grid>
-            <Grid container className='table_row'>
-              <Grid item lg={3}>
-              AFA Yokohama Sub Junior Kids (4 to 8 Years)
-              </Grid>
-              <Grid item lg={3}>
-              Date
-              </Grid>
-              <Grid item lg={3}>
-              Time
-              </Grid>
-              <Grid item lg={3}>
-              Locations
-              </Grid>
-            </Grid>
-        </div>
+      {['January', 'February', 'March'].map(month => renderMonthSection(month))}
     </div>
-  )
+  );
 }
