8af9ed654fdda4be0ab57de06e9494a83bb50f5d
AFAJapan <154581944+AFAJapan@users.noreply.github.com>
2024-04-05 23:52:58 +0900

    Update Yokohama.js

--- src/components/Calendar/Yokohama/Yokohama.js
+++ src/components/Calendar/Yokohama/Yokohama.js
@@ -5,26 +5,23 @@ import { Grid } from '@mui/material';
 export default function TokyoAcademy() {
   // Schedules based on the provided table
   const schedules = {
-    'February': {
-      'Academy': [
-        { date: 'Date  ', time: 'Time  ', location: 'Location' },
-        { date: '3-Feb-2024  ', time: '11:00-13:00 ', location: 'Grandberry' },
-        { date: '4-Feb-2024  ', time: '10:00-12:00', location: 'Kayaba koen' },
-        { date: '11-Feb-2024  ', time: '10:00-12:00', location: 'Kayaba koen' },
-        { date: '17-Feb-2024  ', time: '11:00-13:00', location: 'Grandberry' },
-        { date: '24-Feb-2024  ', time: '11:00-13:00', location: 'Grandberry' },
-       
-      ],
-    },
     'March': {
       'Academy': [
-         { date: 'Date  ', time: 'Time  ', location: 'Location' },
-        { date: '2-Mar-2024  ', time: '11:00-13:00 ', location: 'Grandberry' },
-        { date: '16-Mar-2024  ', time: '11:00-13:00', location: 'Grandberry' },
-        { date: '23-Mar-2024  ', time: '11:00-13:00', location: 'Grandberry' },
-        { date: '30-Mar-2024  ', time: '11:00-13:00', location: 'Grandberry' },
-        
-       
+        { date: 'Date  ', event: 'Event', time: 'Time  ', location: 'Location  ' },
+        { date: '2-Mar-2024  ',event:  'Training', time: '11:00-13:00 ', location: 'Grandberry' },
+        { date: '16-Mar-2024  ',event: 'Training ', time: '11:00-13:00 ', location: 'Grandberry' },
+        { date: '23-Mar-2024  ', event: 'Training  ',time: '11:00-13:00', location: 'Grandberry' },
+        { date: '30-Mar-2024  ', event: 'Training  ',time: '11:00-13:00', location: 'Grandberry' },
+      ],
+    },
+    'April': {
+      'Academy': [
+        { date: 'Date  ', event: 'Event', time: 'Time  ', location: 'Location  ' },
+        { date: '6-Apr-2024  ',event:  '(2 courts)/NOX League', time: '11:00-13:00 ', location: 'Grandberry' },
+        { date: '13-Apr-2024  ',event: 'Training ', time: '09:00-11:00 ', location: 'Kayaba koen' },
+        { date: '20-Apr-2024  ', event: 'Training  ',time: '08:30-10:30', location: 'ShinYokohama' },
+        { date: '21-Apr-2024  ', event: 'Training  ',time: '11:00-13:00', location: 'Kayaba koen' },
+        { date: '27-Apr-2024  ', event: 'Training  ',time: '14:30-17:00', location: 'Kayaba koen' },
       ],
     },
   };
@@ -35,6 +32,7 @@ export default function TokyoAcademy() {
       {teamSchedule.map((session, index) => (
         <div className='table_row' key={`${teamName}-${index}`} style={{ display: 'flex' }}>
           <div className='table_cell' style={{ flex: '1' }}>{session.date}</div>
+          <div className='table_cell' style={{ flex: '1' }}>{session.event}</div>
           <div className='table_cell' style={{ flex: '1' }}>{session.time}</div>
           <div className='table_cell' style={{ flex: '1' }}>{session.location}</div>
         </div>
@@ -55,7 +53,7 @@ export default function TokyoAcademy() {
 
   return (
     <div className='table_container'>
-      {['February', 'March'].map(month => renderMonthSection(month))}
+      {['March', 'April'].map(month => renderMonthSection(month))}
     </div>
   );
 }
